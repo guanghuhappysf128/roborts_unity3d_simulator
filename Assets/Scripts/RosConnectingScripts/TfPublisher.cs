acd0e3125a1cb49daf95bac646228c0664e3e53b
Wenjun Huang <47051053+whuangum@users.noreply.github.com>
2020-01-13 22:38:39 -0500

    tf publisher now uses the correct message type

--- Assets/Scripts/RosConnectingScripts/TfPublisher.cs
+++ Assets/Scripts/RosConnectingScripts/TfPublisher.cs
@@ -1,14 +1,14 @@
 ﻿using System.Threading;
 using UnityEngine;
 using RosSharp;
-using RosOdometry = RosSharp.RosBridgeClient.MessageTypes.Nav.Odometry;
-using RosPoint = RosSharp.RosBridgeClient.MessageTypes.Geometry.Point;
+using RosTfMessage = RosSharp.RosBridgeClient.MessageTypes.Tf2.TFMessage;
+using RosTransformStamped = RosSharp.RosBridgeClient.MessageTypes.Geometry.TransformStamped;
 using RosQuaternion = RosSharp.RosBridgeClient.MessageTypes.Geometry.Quaternion;
 using RosVector3 = RosSharp.RosBridgeClient.MessageTypes.Geometry.Vector3;
 
 namespace Roborts
 {
-    public sealed class TfPublisher : RobortsPublisher<RosOdometry>
+    public sealed class TfPublisher : RobortsPublisher<RosTfMessage>
     {
         private static uint msgSeq = 0;
 
@@ -22,55 +22,40 @@ namespace Roborts
 
         protected override void Start()
         {
+            nameSpace = ""; // tf messages are published without namespace
             base.Start();
 
             // initialization has to be done here because values
             // set in the unity editor aren't visible in the constructor
-            nameSpace = ""; // odom messages are published without namespace
             robotRigidBody = (Rigidbody) robot.GetComponent(typeof(Rigidbody));
         }
 
         private void Update()
         {
-            RosOdometry msg = new RosOdometry();
+            RosTfMessage msg = new RosTfMessage();
 
-            msg.header.frame_id = "base_link";
-            msg.header.seq = msgSeq;
-            msg.child_frame_id = "odom";
-            msg.pose.pose.position = GetRobotPositionRos();
-            msg.pose.pose.orientation = GetRobotOrientationRos();
-            // msg.pose.covariance = null; // TODO: replace this
-            msg.twist.twist.linear = GetRobotLinearVelocity();
-            msg.twist.twist.angular = GetRobotAngularVelocity();
-            // msg.twist.covariance = null; // TODO: replace this
+            msg.transforms = new RosTransformStamped[1] { new RosTransformStamped() };
+            msg.transforms[0].header.frame_id = "/odom";
+            msg.transforms[0].header.seq = msgSeq;
+            msg.transforms[0].child_frame_id = "/base_link";
+            msg.transforms[0].transform.translation = GetRobotPositionRos();
+            msg.transforms[0].transform.rotation = GetRobotOrientationRos();
             msgSeq += 1;
 
             // publish on a separate thread to avoid blocking
-            ThreadPool.QueueUserWorkItem((m) => this.Publish((RosOdometry) m), msg);
+            ThreadPool.QueueUserWorkItem((m) => this.Publish((RosTfMessage) m), msg);
         }
 
-        private RosPoint GetRobotPositionRos()
+        private RosVector3 GetRobotPositionRos()
         {
             Vector3 pos = robot.transform.position.Unity2Ros();
-            return new RosPoint(pos.x, pos.y, pos.z);
+            return new RosVector3(pos.x, pos.y, 0);
         }
 
         private RosQuaternion GetRobotOrientationRos()
         {
             Quaternion pos = robot.transform.rotation.Unity2Ros();
             return new RosQuaternion(pos.x, pos.y, pos.z, pos.w);
-        }
-
-        private RosVector3 GetRobotLinearVelocity()
-        {
-            Vector3 vel = robotRigidBody.velocity.Unity2Ros();
-            return new RosVector3(-vel.y, vel.x, 0);
-        }
-
-        private RosVector3 GetRobotAngularVelocity()
-        {
-            Vector3 vel = robotRigidBody.angularVelocity.Unity2Ros();
-            return new RosVector3(0, 0, -vel.z);
         }
     }
 }
